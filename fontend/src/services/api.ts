3ae2dc788ec131453ce3deaebf715896e538522b
nareshchavan9 <nareshchavan1000@gmail.com>
2025-06-03 00:34:01 +0530

    Update api.ts

--- fontend/src/services/api.ts
+++ fontend/src/services/api.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_URL = 'https://e-ballot-voting-system-1.onrender.com/api';
+//const API_URL = 'https://e-ballot-voting-system-1.onrender.com/api';
+const API_URL = 'https://e-ballot-voting-system-1.onrender.com';
+
 
 // Create axios instance
 const api = axios.create({
